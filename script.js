40c78ce093c0c5122cb1adc8d9912944964c1348
bophancungung <bophancungung@gmail.com>
2021-08-06 14:53:13 +0700

    adding function with javascript

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-// alert('helllllooooo');
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+
+// Prompt the user to select media stream, pass to video elemnt, the play
+async function selectMediaStream() {
+  try {
+    const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+    videoElement.srcObject = mediaStream;
+    videoElement.onloadedmetadata = () => {
+      videoElement.play();
+    }
+  } catch (error) {
+    // Catch error here
+    console.log('whoops, error selectMediaStream:', error);
+  }
+}
+
+button.addEventListener('click', async () => {
+  // Disable button
+  button.disabled = true;
+  // Start picture in picture
+  await videoElement.requestPictureInPicture();
+  // Reset button
+  button.disabled = false;
+})
+
+// On load
+selectMediaStream();
